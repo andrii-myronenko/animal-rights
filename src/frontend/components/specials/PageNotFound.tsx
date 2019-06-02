ce843417b6ee957de37f62f2095799c8d2a0c78e
AndrewMS <andrewms2013@icloud.com>
2019-06-02 03:32:27 +0300

    update not found page

--- src/frontend/components/specials/PageNotFound.tsx
+++ src/frontend/components/specials/PageNotFound.tsx
@@ -1,9 +1,24 @@
-import * as React from 'react';
+import * as React from "react";
+import { createStyles, withStyles, WithStyles, Theme } from '@material-ui/core/styles';
 
-export default class PageNotFound extends React.Component {
+const styles = ({ spacing }: Theme) => createStyles({
+    container: {
+        margin: spacing.unit * 3
+    },
+});
+
+export interface Props extends WithStyles<typeof styles> {
+}
+
+class PageNotFound extends React.Component<Props> {
+    
     render() {
         return (
-            <h1>Not found</h1>
+            <div className={this.props.classes.container}>
+                <h1>Page not found</h1>
+            </div>
         );
-    }
-}
+    }  
+  }
+
+export default withStyles(styles)(PageNotFound);
