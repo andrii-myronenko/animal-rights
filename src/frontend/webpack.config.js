deac86bedebbab7b96e806a14085c1a884c92f24
AndrewMS <andrewms2013@icloud.com>
2019-06-06 14:18:05 +0300

    autoprefixer

--- src/frontend/webpack.config.js
+++ src/frontend/webpack.config.js
@@ -67,8 +67,18 @@ module.exports = {
             {
                 test: /\.css$/,
                 use: [
-                  { loader: "style-loader" },
-                  { loader: "css-loader" }
+                    { loader: "style-loader" },
+                    { loader: "css-loader" },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: function () {
+                                return [
+                                    require('autoprefixer')
+                                ];
+                            }
+                        }
+                    }
                 ]
             }
         ]
